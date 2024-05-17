c984e1ad286e195507f6a9a238dad05ff450a0bf
thomassmith0526 <thomassmith0526@gmail.com>
2024-05-17 18:10:29 -0500

    what what

--- assete/js/blog.js
+++ assete/js/blog.js
@@ -4,12 +4,15 @@ let blogArray = JSON.parse(localStorage.getItem('blogtext'));
 
 
 
-function creattext(object) {
+function createtext(object) {
   console.log(object)
   
 
   const blogText  = document.querySelector('#blogText')
-  let contentElement = document.createElement('section')
+  let contentEl = document.createElement('section')
+  // const newDiv = document.createElement('div')
+  // newDiv.textContent = p0, p1, p2
+  // newDiv.style.border = "2px solid red"
 
   let p0 = document.createElement('p')
   let p1 = document.createElement('p')
@@ -26,15 +29,44 @@ function creattext(object) {
   p1.setAttribute('class','content')
   p2.setAttribute('class', 'username')
 
-  contentElement.appendChild(p0)
-  contentElement.appendChild(p1)
-  contentElement.appendChild(p2)
-  blogText.appendChild(contentElement);
+  contentEl.appendChild(p0)
+  contentEl.appendChild(p1)
+  contentEl.appendChild(p2)
+  blogText.appendChild(contentEl);
+  blogText.style.border = "5px solid red"
+  blogText.style.marginTop = "1%"
+  p0.style.margin = "1%"
+  p1.style.margin = "1%"
+  p2.style.margin = "1%"
+  p0.style.borderBottom = "2px solid red"
 
-
+  
 
   // for (let i = 0; i< blogArray.length; i++) {
   //   createPost(blogArray[i])
-//   }
+  // }
 }
-creattext(blogArray)
+createtext(blogArray)
+
+// let blogArraytwo = JSON.parse(localStorage.getItem('blogtext'))
+
+// function createblog(object){
+  
+
+//   const blogtwo = document.querySelector('#blogtwo')
+//   let contenttwo = document.createElement('section')
+
+//   let p3 = document.createElement('p') 
+//   let p4 = document.createElement('p')
+//   let p5 = document.createElement('p')
+
+//   p3.textContent = object.username
+//   p4.textContent = object.title
+//   p5.textContent = object.content
+
+//   contenttwo.appendChild(p3)
+//   contenttwo.appendChild(p4)
+//   contenttwo.appendChild(p5)
+//   blogtwo.appendChild(contenttwo)
+// }
+// createblog(blogArraytwo)
